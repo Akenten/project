3cc9a3ad64d9e7c695503c6d470147770689bf4c
Akenten <ars.alex19@yandex.ru>
2021-05-13 22:50:06 +0300

    third commit

--- C_prog/C_prog/C_prog.cpp
+++ C_prog/C_prog/C_prog.cpp
@@ -1,6 +1,11 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int doubler(int i)
+{
+	return i * 2;
+}
+
 int main()
 {
 	printf("hi\n");
@@ -8,5 +13,10 @@ int main()
 	for (int i = 0; i < q; ++i)
 	{
 		printf("%d\t", i);
+	}
+	printf("\n");
+	for (int i = 0; i < q; ++i)
+	{
+		printf("%d\t", doubler(i));
 	}
 }
